57b489c79af277a9ba03e0e326fb4b6975eca213
Dzmitry Shushkevich <90650280+sp1se19@users.noreply.github.com>
2022-04-29 13:19:29 +0300

    refactor: refactor build and config files

--- web/src/main/java/com/innowise/web/config/SpringConfig.java
+++ web/src/main/java/com/innowise/web/config/SpringConfig.java
@@ -1,14 +1,10 @@
 package com.innowise.web.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ComponentScan("com.innowise.web")
-@PropertySource("classpath:projectParams.properties")
 public class SpringConfig {
 
     @Bean
