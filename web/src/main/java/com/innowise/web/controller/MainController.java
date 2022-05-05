24fa1fccb9abf058ae5bfea02b928ca183a3f660
Dzmitry Shushkevich <90650280+sp1se19@users.noreply.github.com>
2022-05-05 19:25:53 +0300

    refactor: use lombok annotations

--- web/src/main/java/com/innowise/web/controller/MainController.java
+++ web/src/main/java/com/innowise/web/controller/MainController.java
@@ -1,21 +1,19 @@
 package com.innowise.web.controller;
 
 import com.innowise.web.project.ProjectParams;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class MainController {
 
     private final ProjectParams params;
-
-    @Autowired
-    public MainController(ProjectParams params) {
-        this.params = params;
-    }
 
     @GetMapping("/about")
     public ProjectParams getInfoAboutProject() {
--- web/src/main/java/com/innowise/web/project/ProjectParams.java
+++ web/src/main/java/com/innowise/web/project/ProjectParams.java
@@ -1,20 +1,12 @@
 package com.innowise.web.project;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Component
+@ConfigurationProperties(prefix = "project")
+@Getter
 public class ProjectParams {
 
-    @Value("${applicationName}")
     private String applicationName;
-    @Value("${version}")
     private String version;
-
-    public String getVersion() {
-        return version;
-    }
-    public String getApplicationName() {
-        return applicationName;
-    }
 }
