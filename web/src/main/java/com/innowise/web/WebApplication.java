0f98e5fb5c4d0d0ed3eec54322dab7011c10548f
Dzmitry Shushkevich <90650280+DzimaSh@users.noreply.github.com>
2022-05-06 19:57:37 +0300

    CTB-2 (#2)
    add: add logic that fetching data about project from server
    
    Co-authored-by: Dzmitry Shushkevich <90650280+sp1se19@users.noreply.github.com>

--- web/src/main/java/com/innowise/web/WebApplication.java
+++ web/src/main/java/com/innowise/web/WebApplication.java
@@ -1,9 +1,12 @@
 package com.innowise.web;
 
+import com.innowise.web.project.ProjectParams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ProjectParams.class})
 public class WebApplication {
 
     public static void main(String[] args) {
