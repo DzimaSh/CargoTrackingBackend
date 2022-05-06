71b6d2a1d81140aa5c9d3d106047538597ff32ab
Dzmitry Shushkevich <90650280+sp1se19@users.noreply.github.com>
2022-05-06 16:49:25 +0300

    fix: fix lombok configuration

--- web/src/main/java/com/innowise/web/controller/MainController.java
+++ web/src/main/java/com/innowise/web/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.innowise.web.controller;
 
 import com.innowise.web.project.ProjectParams;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- web/src/main/java/com/innowise/web/project/ProjectParams.java
+++ web/src/main/java/com/innowise/web/project/ProjectParams.java
@@ -1,10 +1,11 @@
 package com.innowise.web.project;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "project")
-@Getter
+@Getter @Setter
 public class ProjectParams {
 
     private String applicationName;
